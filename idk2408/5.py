6fc018dedaa0b16f54a8c310499242d50ccacb80
ChristianRivero <christianriveroarnez@gmail.com>
2022-08-25 17:11:09 -0400

    intento de 5

--- idk2408/5.py
+++ idk2408/5.py
@@ -1,48 +1,63 @@
-
-
-from asyncio.windows_events import NULL
-from configparser import InterpolationMissingOptionError
-from contextlib import nullcontext
-from mimetypes import init
-
+import sys
+from xml.sax.xmlreader import InputSource
 
 class node:
     def __init__(self,val):
         self.Value=val
-        self.Aristas=[]
+        self.Aristas=set()
         pass
     def addAnEdge(self,destino):
-        self.Aristas.append(destino)
+        self.Aristas.add(destino)
+    def deleteAnEdge(self,destino):
+        self.Aristas.remove(destino)
     def getEdges(self):
         return self.Aristas
-
 
 def addNode(value,graph):
     graph[value]=node(val=value)
 def addEdgeToGraph(Val1,Val2,graph):
     graph[Val1].addAnEdge(Val2)
     graph[Val2].addAnEdge(Val1)
+def deleteEdge(Val1,Val2,graph):
+    graph[Val1].deleteAnEdge(Val2)
+    graph[Val2].deleteAnEdge(Val1)
 
-def findConnections(initNode,graph,connectedNodes:set):
+def findConnections(initNode,graph,connectedNodes:set,CitiesNodes:set,lowLimPowerSupply):
+
     for i in graph[initNode].getEdges():
         if i not in connectedNodes:
             connectedNodes.add(i)
-            findConnections(i,graph,connectedNodes)
+            if i <lowLimPowerSupply:
+                CitiesNodes.add(i)
+            findConnections(i,graph,connectedNodes,CitiesNodes,lowLimPowerSupply)
         
 
-def GetNodesConnectedToPowerPlant(graph):
+def GetNodesConnectedToPowerPlant(graph,lowLimPowerSupply,TopLimPowerSupply):
+    MaxCities=lowLimPowerSupply-1
+    CitiesNodes=set()
     connectedNodes=set()
+    for i in range(lowLimPowerSupply,TopLimPowerSupply+1):
+        findConnections(i,graph,connectedNodes,CitiesNodes,lowLimPowerSupply)
+        if len(CitiesNodes)==MaxCities:
+            break
+    return len(CitiesNodes)
+            
+
 
 def main():
     graph={}
-    addNode(3,graph)
-    addNode(4,graph)
-    addNode(5,graph)
-    addNode(6,graph)
-    addEdgeToGraph(3,4,graph)
-    addEdgeToGraph(4,5,graph)
-    connectedNodes=set()
-    findConnections(3,graph,connectedNodes)
-    print(connectedNodes)
-    
+    N,M,E=map(int,sys.stdin.readline().split(" "))
+    Connections=[list(map(int, input().split())) for _ in range(E)] 
+    Q=int(sys.stdin.readline())
+    Eventos=[int(sys.stdin.readline()) for _ in range(Q)]
+    for i in range(1,N+M+1):
+        addNode(i,graph)
+    for i in Connections:
+        addEdgeToGraph(i[0],i[1],graph)
+
+    for i in Eventos:
+        deleteEdge(Connections[i-1][0],Connections[i-1][1],graph)
+        print(GetNodesConnectedToPowerPlant(graph,N+1,N+M))
+
+
 main()
